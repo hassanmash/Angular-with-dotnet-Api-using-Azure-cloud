b26450cf860002b9e8302e7949df9d2f70706090
Hassan <61452898+hassanmash@users.noreply.github.com>
2023-06-01 13:43:02 +0530

    Add files via upload

---
+++ api/AzureQueueStorage/Controllers/QueuesController.cs
@@ -0,0 +1,101 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureQueueStorage.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QueuesController : ControllerBase
+    {
+        private IConfiguration config;
+        private string connstring = "";
+        public QueuesController(IConfiguration config)
+        {
+            this.config = config;
+            this.connstring = this.config.GetConnectionString("QueueConnectionString");
+        }
+
+        [HttpPost]
+        [Route("createQueue/{queuename}")]
+        public IActionResult CreateQueue(string queuename)
+        {
+            QueueClient queue = new QueueClient(this.connstring, queuename);
+            if(queue.Exists().Value)
+            {
+                var res = new
+                {
+                    text = "Queue '" + queuename + "' exists!"
+                };
+                return Conflict(res);
+            }
+            queue.Create();
+            return Ok(new {
+                text = "Queue '" + queuename + "' created successfully!"
+            });
+        }
+
+        [HttpGet]
+        [Route("sendmessage/{queuename}/{message}")]
+        public IActionResult SendMessage(string queuename, string message)
+        {
+            QueueClient queue = new QueueClient(this.connstring, queuename);
+            if(!queue.Exists().Value)
+            {
+                return NotFound(new
+                {
+                    text = "Queue '" + queuename + "' Not Found!"
+                });
+            }
+            queue.SendMessage(message);
+            return Ok(new
+            {
+                text = "Message sent to '" + queuename + "' successfully"
+            });
+        }
+
+        [HttpGet]
+        [Route("readmessage/{queuename}")]
+        public IActionResult ReadMessage(string queuename)
+        {
+            QueueClient queue = new QueueClient(this.connstring, queuename);
+            if (!queue.Exists().Value)
+            {
+                return NotFound(new
+                {
+                    text = "Queue '" + queuename + "' Not Found!"
+                });
+            }
+            QueueMessage queueMessage = queue.ReceiveMessage().Value;
+            if(queueMessage == null)
+            {
+                return NotFound(new
+                {
+                    text = "No Messages to read!"
+                });
+            }
+            queue.DeleteMessage(queueMessage.MessageId, queueMessage.PopReceipt);
+            return Ok(queueMessage.Body.ToString());
+        }
+
+        [HttpDelete]
+        [Route("delete/{queuename}")]
+        public IActionResult DeleteQueue(string queuename)
+        {
+            QueueClient queue = new QueueClient(this.connstring, queuename);
+            if (!queue.Exists().Value)
+            {
+                return NotFound(new
+                {
+                    text = "Queue '" + queuename + "' Not Found!"
+                });
+            }
+            queue.Delete();
+            return Ok(new
+            {
+                text = "Queue '" + queuename + "' deleted!"
+            });
+        }
+    }
+}
---
+++ api/AzureTableStorage/Controllers/TablesController.cs
@@ -0,0 +1,91 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureTableStorage.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TablesController : ControllerBase
+    {
+        private IConfiguration config;
+        private string connstring = "";
+        private string tablename = "Users";
+        public TablesController(IConfiguration config)
+        {
+            this.config = config;
+            this.connstring = this.config.GetConnectionString("tableConnectionString");
+        }
+
+        [HttpPost]
+        [Route("adduser")]
+        public IActionResult AddUser(User u)
+        {
+            TableClient tc = new TableClient(connstring,tablename);
+            try
+            {
+                tc.AddEntity(u);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500,ex.Message);
+            }
+            return Ok(new
+            {
+                text = "Added entity with user ID: '"+u.RowKey +"' in location: '" + u.PartitionKey + "'"
+            });
+        }
+
+        [HttpGet]
+        [Route("{pk}/{rk}")]
+        public IActionResult GetUser(string pk, string rk)
+        {
+            TableClient tc = new TableClient(connstring,tablename);
+            User? u = null;
+            try
+            {
+                u = tc.GetEntity<User>(pk, rk);
+            }
+            catch(Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+            return Ok(u);
+        }
+
+        [HttpGet]
+        [Route("allusers")]
+        public IActionResult GetAllUsers()
+        {
+            TableClient tc = new TableClient(connstring,tablename);
+            return Ok(tc.Query<User>().ToList());
+        }
+
+        [HttpGet]
+        [Route("location/{location}")]
+        public IActionResult GetUserbyLocation(string location)
+        {
+            TableClient tc = new TableClient(connstring,tablename);
+            var result = tc.Query<User>(u => u.PartitionKey == location);
+
+            if (result.Count() == 0)
+                return NotFound(new
+                {
+                    text = "No Users found for location: " + location
+                });
+
+            return Ok(result.ToList());
+        }
+    }
+
+    public class User: ITableEntity
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsLoginAllowed { get; set; }
+        public string ?PartitionKey { get; set; }
+        public string ?RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
